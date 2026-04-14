666e3d03f725ca205a7ecf0c53dfa18f7a00930f
Silvi1224 <silviaislam159@gmail.com>
2026-04-14 23:12:12 +0600

    api_server.py

--- api_server.py
+++ api_server.py
@@ -108,5 +108,8 @@ def recommend():
 
 # Run server
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
